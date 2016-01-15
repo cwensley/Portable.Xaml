e3290e56b8952c81c032a72093c9d2c93b7dc696
Curtis Wensley <curtis.wensley@gmail.com>
2016-01-14 17:58:51 -0800

    Updating to 0.9.0-alpha

--- src/Portable.Xaml/Assembly/AssemblyInfo.cs
+++ src/Portable.Xaml/Assembly/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Security.Permissions;
 [assembly: CLSCompliant (true)]
 //[assembly: ComVisible (false)]
 
-[assembly: AssemblyVersion ("0.8.0.0")]
-[assembly: AssemblyInformationalVersion ("0.8.0")]
+[assembly: AssemblyVersion ("0.8.*")]
+[assembly: AssemblyInformationalVersion ("0.9.0-alpha")]
 [assembly: AssemblyCompany("Mono development team")]
 [assembly: AssemblyCopyright("(c) Various Mono authors")]
 
