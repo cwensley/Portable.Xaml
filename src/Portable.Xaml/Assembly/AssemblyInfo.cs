66929b91a32c58b8fe327eff03458519e592cabe
Curtis Wensley <curtis.wensley@gmail.com>
2016-11-16 16:56:18 -0800

    Updating to 0.16.0-alpha

--- src/Portable.Xaml/Assembly/AssemblyInfo.cs
+++ src/Portable.Xaml/Assembly/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Security.Permissions;
 [assembly: CLSCompliant (true)]
 //[assembly: ComVisible (false)]
 
-[assembly: AssemblyVersion ("0.15.0.0")]
-[assembly: AssemblyInformationalVersion ("0.15.0")]
+[assembly: AssemblyVersion ("0.15.*")]
+[assembly: AssemblyInformationalVersion ("0.16.0-alpha")]
 [assembly: AssemblyCompany("Mono development team")]
 [assembly: AssemblyCopyright("(c) Various Mono authors")]
 
